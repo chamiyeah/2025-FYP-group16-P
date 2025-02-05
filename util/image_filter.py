cdb09a9f15ee9027f258951e0aa1d82d84c64282
chamiyeah <gachampika@live.com>
2025-02-05 15:10:08 +0100

    Updated folder structure. commented the code

--- util/image_filter.py
+++ util/image_filter.py
@@ -5,15 +5,16 @@ import csv
 #this script goes through the data students csv and filter out the images which are corrosponding to our group.
 #and then it put in a subfolder and make a new csv .
 
-data_dir = os.path.join("data")
-csv_file = os.path.join( "data-student.csv")
-output_csv = os.path.join("filtered_images.csv")
-output_dir = os.path.join("filtered_images")
+#path setup.
+data_dir = os.path.join("data") #input images folder
+csv_file = os.path.join( "data-student.csv") #input csv for the image to group list
+output_csv = os.path.join("filtered_images.csv") #output path for csv
+output_dir = os.path.join("filtered_images") #output directory filtered images
 
-
+#folder check for if exist already
 os.makedirs(output_dir, exist_ok=True)
 
-
+#filter images acording to the csv
 filtered_images = []
 with open(csv_file, mode='r') as file:
     reader = csv.reader(file)
@@ -23,7 +24,7 @@ with open(csv_file, mode='r') as file:
             filtered_images.append(filename)
             shutil.copy(os.path.join(data_dir, filename), output_dir)
 
-
+#create csv with filterd image file names
 with open(output_csv, mode='w', newline='') as file:
     writer = csv.writer(file)
     for image in filtered_images:
